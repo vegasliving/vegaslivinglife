cab5161aecdb965525a71d933cb78f4da08f397d
Troy Do <hoangdov@gmail.com>
2017-06-04 18:29:47 -0700

    modify download

--- properties/download.py
+++ properties/download.py
@@ -6,8 +6,9 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 def downloadImage(matrixUniqueID,i):
 	print(i)
 	for count in range(0,int(i)):
+		print(count)
 		url = 'http://rets.las.mlsmatrix.com/rets/GetObject.ashx?Type=LargePhoto&Resource=Property&ID=%s:%s'%(matrixUniqueID, count)
-		path = 'static/properties/images/%s-%s.jpg'%(matrixUniqueID,count)
+		path = 'static/properties/images/%s:%s.jpg'%(matrixUniqueID,count)
 		gpath = path[7:32]
 		r = requests.get(url, auth=HTTPDigestAuth('tinoco', 'lv360'), stream=True)
 		print(r.headers, r.status_code, r.raw)
