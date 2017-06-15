d996e04362233e2a1ef5ce0d0edd6a98475e9fe3
Troy Do <hoangdov@gmail.com>
2017-06-15 13:02:16 -0700

    Lead Form looking good

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class PodioForm(forms.Form):
-	firstName= forms.CharField(label="First Name", max_length=300, widget = forms.TextInput(attrs={"class":"mdl-textfield__input"}))
-	lastName = forms.CharField(label="Last Name", max_length=300, widget = forms.TextInput(attrs={"class":"mdl-textfield__input"}))
-	email = forms.EmailField(label="Email",  widget = forms.TextInput(attrs={"class":"mdl-textfield__input"}))
-	phone = forms.IntegerField(label="Phone", widget = forms.TextInput(attrs={"class":"mdl-textfield__input"}))
+	firstName= forms.CharField(label="First Name", max_length=300, widget = forms.TextInput(attrs={"class":"mdl-textfield__input", "style":"width: 100%; border-bottom: 2px solid #2d3739; margin: 5px 0px"}))
+	lastName = forms.CharField(label="Last Name", max_length=300, widget = forms.TextInput(attrs={"class":"mdl-textfield__input", "style":"width: 100%; border-bottom: 2px solid #2d3739; margin: 5px 0px" }))
+	email = forms.EmailField(label="Email",  widget = forms.TextInput(attrs={"class":"mdl-textfield__input", "style":"width: 100%; border-bottom: 2px solid #2d3739; margin: 5px 0px"}))
+	phone = forms.IntegerField(label="Phone", widget = forms.TextInput(attrs={"class":"mdl-textfield__input", "style":"width: 100%; border-bottom: 2px solid #2d3739; margin: 5px 0px"}))
